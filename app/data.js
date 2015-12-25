07a9af4bce00d1dd3e9dcff25bffba851af13311
jiubao <xia.tlbb@gmail.com>
2015-12-25 14:21:34 +0800

    miniquiz

---
+++ app/data.js
@@ -0,0 +1,21 @@
+var codes = {
+
+};
+
+var quizzes = [{
+		code:
+"(function(){\
+    return typeof arguments;\
+})();",
+		options: ["object", "array", "arguments", "undefined"],
+		index: 0
+	}, {
+		code:
+"var f = function g(){ return 23; };\
+typeof g();",
+		options: ["number", "undefined", "function", "Error"],
+		index: 3
+	}
+
+]
+;
---
+++ app/miniquiz.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    quizzes.forEach(function(quiz, index) {
+    	quiz.title = "Question " + (index + 1);
+    	quiz.group = "group" + index;
+    	// quiz.code = unescape(quiz.code);
+    	quiz.options = quiz.options.map(function(opt, oi) {
+    		return {
+    			id: "option" + index + '' + oi,
+    			value: opt,
+    			hit: oi === quiz.index ? 1 : 0
+    		};
+    	});
+    });
+
+    var cardtpl = $('#tpl-card').html();
+    cardtpl = decodeEntities(cardtpl);
+    var $container = $('.container');
+    quizzes.forEach(function(quiz) {
+    	var card = ejs.render(cardtpl, quiz);
+    	$container.append(card);
+    });
+
+	$('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      //expandable: true
+    });
+
+    $('.result-right').hide();
+    $('.result-wrong').hide();
+
+    $('input[type="radio"]').on('change', function() {
+    	var $this = $(this);
+    	// var group = $this.attr('name');
+    	var hit = $this.data('hit') == 1;
+
+    	if (hit) {
+    		$this.siblings('.result-right').show();
+    		$this.siblings('.result-wrong').hide();
+    	} else {
+    		$this.siblings('.result-right').hide();
+    		$this.siblings('.result-wrong').show();
+    	}
+
+    	return false;
+    });
+
+    $('.toggleSpec').on('click', function() {
+    	$(this).closest('.actions').siblings('.spec').toggleClass('show');
+    });
+});
+
+function decodeEntities(encodedString) {
+    var textArea = document.createElement('textarea');
+    textArea.innerHTML = encodedString;
+    return textArea.value;
+}
